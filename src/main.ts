b9aa0250199732a8ebb94eb0da2a70fd6ce33d1f
Abdiel49 <abdielorellana3@gmail.com>
2025-09-30 23:10:48 -0400

    feat - add imports and use new routers

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,24 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import router from './routes';
+import helthRoter from './helthCheck/helthCheck.route';
+import dotenv from 'dotenv'
+import VoteRouter from './votos/votos.routes';
 
-const PORT = 3003;
+dotenv.config()
+
+console.log('process.env.PORT', process.env.PORT)
+
+const PORT = process.env.PORT || 3000;
 
 const app = express()
+
 
 app.use(bodyParser.json())
 // app.use(bodyParser.urlencoded({ extended: true }))
 app.use('/', router)
+app.use('/', helthRoter)
+app.use('/', VoteRouter)
 
 app.get('/test-ts', (req, res) => {
   res.send('Typescript funciona!')
